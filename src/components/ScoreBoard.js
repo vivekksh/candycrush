d54adfe2b0dbe99d66488fcde1615a6b9e57866b
vyyvek <beingviveksharma99@gmail.com>
2024-07-13 16:25:59 +0530

    trying

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,10 @@
+import '../index.css'
+const ScoreBoard = ({ score }) => {
+    return (
+      <div className="score-board">
+        <h2>Score:{score}</h2>
+      </div>
+    )
+  }
+  
+  export default ScoreBoard
